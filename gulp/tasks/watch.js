a6cd8d25336c1a5a77905624ae600004f66b398c
Miranda <miranda.pegg@gmail.com>
2019-08-01 13:33:54 -0500

    Completed finishing touches

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,22 +3,23 @@ var gulp = require('gulp'),
 	browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
-	
+
 	browserSync.init({
 		notify: false,
 		server: {
 			baseDir: "app"
-		}
+		},
+		tunnel: true
 	});
-	
+
 	watch('./app/index.html', function() {
 		browserSync.reload();
 	});
-	
+
 	watch('./app/assets/styles/**/*.css', function() {
 		gulp.start('cssInject');
 	});
-	
+
 	watch('./app/assets/scripts/**/*.js', function() {
 		gulp.start('scriptsRefresh');
 	});
